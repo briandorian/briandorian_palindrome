7156efca2d0ee8cfd032fc503a1fb9d8244b6693
Brian Acosta <bacosta70@gmail.com>
2021-10-31 17:51:59 +0100

    Initialize repository

---
+++ lib/briandorian_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "briandorian_palindrome/version"
+
+module BriandorianPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/briandorian_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BriandorianPalindrome
+  VERSION = "0.1.0"
+end
