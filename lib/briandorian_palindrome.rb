41cba74505e0daf6e90b424a4aded7d3db60be26
Brian Acosta <bacosta70@gmail.com>
2021-10-31 18:28:17 +0100

    Added the code on the lib part and the tests

--- lib/briandorian_palindrome.rb
+++ lib/briandorian_palindrome.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
-require_relative "briandorian_palindrome/version"
+require "briandorian_palindrome/version"
 
-module BriandorianPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.downcase
+    end
 end
